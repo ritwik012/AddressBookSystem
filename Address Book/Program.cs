cfe410812b1669c056d2b5efee4cd9519143e33f
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-17 00:11:17 +0530

    [Rutwik]Added:ContactsCreated AddressBookSystem Features Implemented

---
+++ Address Book/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to the Address Book Program");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contact \n 4. Delete Contact \n 5. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
+                        Contact contact = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
